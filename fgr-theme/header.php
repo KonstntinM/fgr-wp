ac6c6aad966eee7e7ae645f7ee3b9d111ba07461
Konstantin <64841866+KonstntinM@users.noreply.github.com>
2024-03-13 16:32:27 +0100

    Add data-no-translation label to custom language switcher to fix bug

--- fgr-theme/header.php
+++ fgr-theme/header.php
@@ -73,9 +73,15 @@
                         <div class="vn-header-search">
                             <span <?php echo viral_news_amp_search_toggle(); ?>><i class="mdi-magnify"></i></span>
                         </div>
-                        <div class="fgr-lang-menu-mobile">
+                        <div class="fgr-lang-menu-mobile" data-no-translation>
                             <?php
-                            $sprachen = trp_custom_language_switcher();
+                                $current_url_path = $_SERVER['REQUEST_URI'];
+                                $sprachen = trp_custom_language_switcher();
+                    
+                                // Remove the current language from the list
+                                $sprachen = array_filter($sprachen, function ($item) use ($current_url_path) {
+                                    return !str_starts_with($current_url_path, '/'. $item['short_language_name']);
+                                });
                             ?>
                             <?php // if ( apply_filters( 'trp_allow_tp_to_run', true ) ){ 
                             ?>
@@ -83,11 +89,11 @@
                                 // Check if the current language is the current page language
                                 global $wp;
 
-                                if (!str_starts_with($wp->request, $item['language_code'])) { ?>
+                                ?>
                                     <a href="<?php echo $item['current_page_url'] ?>" class="tp-language-name">
                                         <span><?php echo $item['short_language_name'] ?></span>
                                     </a>
-                                <?php }
+                                <?php
                                 // }
                                 ?>
                             <?php } ?>
@@ -115,21 +121,26 @@
                         <div class="vn-header-search">
                             <span <?php echo viral_news_amp_search_toggle(); ?>><i class="mdi-magnify"></i></span>
                         </div>
-                        <div class="fgr-lang-menu-mobile">
+                        <div class="fgr-lang-menu-mobile" data-no-translation>
                             <?php
-                            $sprachen = trp_custom_language_switcher();
+                                $current_url_path = $_SERVER['REQUEST_URI'];
+                                $sprachen = trp_custom_language_switcher();
+                    
+                                // Remove the current language from the list
+                                $sprachen = array_filter($sprachen, function ($item) use ($current_url_path) {
+                                    return !str_starts_with($current_url_path, '/'. $item['short_language_name']);
+                                });
                             ?>
                             <?php // if ( apply_filters( 'trp_allow_tp_to_run', true ) ){ 
                             ?>
                             <?php foreach ($sprachen as $name => $item) {
                                 // Check if the current language is the current page language
                                 global $wp;
-
-                                if (!str_starts_with($wp->request, $item['language_code'])) { ?>
+                                ?>
                                     <a href="<?php echo $item['current_page_url'] ?>" class="tp-language-name">
                                         <span><?php echo $item['short_language_name'] ?></span>
                                     </a>
-                                <?php }
+                                <?php
                                 // }
                                 ?>
                             <?php } ?>
--- fgr-theme/hooks/viral-news-search-icon.php
+++ fgr-theme/hooks/viral-news-search-icon.php
@@ -1,7 +1,7 @@
 <?php
 
 /**
- * This file hooks into the code displaying the Search Icon in the header to add our language menu.
+ * This file hooks into the code displaying the Search Icon in the header to add our language menu to display our custom language switcher.
  * 
  * IMPORTANT! Damit dieses Snippet funktioniert muss das Plugin "TranslatePress" installiert sein.
  * Docs: https://translatepress.com/docs/developers/custom-language-switcher/
@@ -13,25 +13,28 @@ function viral_news_search_icon()
 {
 ?>
     <div class="fgr-right-header">
-        <div style="display: inline-flex; gap: 10px; align-items: center;">
+        <div style="display: inline-flex; gap: 10px; align-items: center;" data-no-translation>
 
             <?php
 
+            $current_url_path = $_SERVER['REQUEST_URI'];
             $sprachen = trp_custom_language_switcher();
+
+            // Remove the current language from the list
+            $sprachen = array_filter($sprachen, function ($item) use ($current_url_path) {
+                return !str_starts_with($current_url_path, '/'. $item['short_language_name']);
+            });
+
             ?>
             <?php // if ( apply_filters( 'trp_allow_tp_to_run', true ) ){ 
             ?>
-            <?php foreach ($sprachen as $name => $item) {
+            <?php foreach ($sprachen as $item) {
                 // Check if the current language is the current page language
-                global $wp;
-
-                if (!str_starts_with($wp->request, $item['language_code'])) { ?>
+                ?>
                     <a href="<?php echo $item['current_page_url'] ?>" class="tp-language-name">
                         <span><?php echo $item['short_language_name'] ?></span>
-                        <span><?php echo $wp->request ?></span>
                     </a>
-                <?php }
-                // }
+                <?php 
                 ?>
             <?php } ?>
             <div class="vn-header-search">
