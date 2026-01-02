16842c15b4bf37eab87ecc234227d538a8af9997
konstantin <konstantinm@posteo.de>
2026-01-03 00:18:12 +0100

    try to create (currently not working) dev container

---
+++ .devcontainer/wordpress-container-setup.sh
@@ -0,0 +1,38 @@
+echo "✨ Starting WP-Container-Setup Script"
+
+cd /var/www/html || exit
+
+# Global Site Config
+SITE_TITLE="Generation-Reports"
+PLUGINS="updraftplus"
+
+# Admin User
+ADMIN_USER="admin"
+ADMIN_PASS="admin"
+ADMIN_EMAIL="admin@admin.com"
+
+# Setup WordPress
+if [ ! -f wp-config.php ]; then
+    echo "🛒 Installing WordPress"
+    wp core download --locale=de_DE
+    echo "🧪 Configuring WordPress";
+    # Wait for DB to be ready
+    echo "⏳ Waiting for Database connection..."
+    # Loop until WP-CLI can connect to the DB host
+    until wp db check --dbhost="db" --dbuser="wp_user" --dbpass="wp_pass" > /dev/null 2>&1; do
+      echo -n "."
+      sleep 3
+    done
+    echo " Connected!"
+    wp config create --skip-check --dbhost="db" --dbname="wordpress" --dbuser="wp_user" --dbpass="wp_pass";
+    echo "- 📖 Configured Database access"
+    wp core install --title="$SITE_TITLE" --url="http://localhost:8080" --admin_user="$ADMIN_USER" --admin_email="$ADMIN_EMAIL" --admin_password="$ADMIN_PASS" --skip-email;
+    echo "- 🧱 Configured Global Options & Admin User"
+    wp plugin install $PLUGINS --activate
+    echo "- 🛍️ Installed and activated the following plugins: $PLUGINS"
+
+    echo "✅ Configuration completed."
+
+else
+    echo "🍟 WordPress seems to be already configured."
+fi
